1180fdaba0d5c4746d753872ac3b7e2b54346172
tblagarrett <tblagarrett@gmail.com>
2024-11-02 00:34:40 -0700

    L16 complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-// todo
+// @deno-types="npm:@types/leaflet@^1.9.14"
+// import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+// import luck from "./luck.ts";
+
+const button = document.createElement("button");
+button.textContent = "Click Me!";
+button.id = "myButton";
+button.className = "btn";
+
+button.addEventListener("click", () => {
+  alert("Button clicked!");
+});
+
+document.body.appendChild(button);
